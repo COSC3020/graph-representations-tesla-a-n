3ec74d455725586f63f19385016249982ac0754f
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-10 04:04:40 -0600

    bonus function

--- code.js
+++ code.js
@@ -5,3 +5,13 @@ function convertToAdjList(adjMatrix) {
             push 1
     return adjList;
 }
+
+function convertToAdjMatrix(adjList) {
+    adjMatrix of 0
+    outerloop for each vertex
+        innerloop for each vertex
+            neighbor = adjMatrix[i][j]
+            push 0 at neighbor
+            push 1 at adjMatrix[i]neighbor
+    return adjMatrix
+}
