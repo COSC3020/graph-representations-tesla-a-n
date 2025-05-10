590ee181ccd818bdeccfa3f96c4b446d3e6ae073
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-10 04:39:30 -0600

    Update code.js

--- code.js
+++ code.js
@@ -13,11 +13,16 @@ function convertToAdjList(adjMatrix) {
 
 function convertToAdjMatrix(adjList) {
     const adjMatrix = new Array(adjList.length).fill().map(() => new Array(adjList.length).fill(0));
+    const neighbor = adjList[i][j];
     for (let i = 0; i < adjList.length; i++) {
         adjMatrix[i] = [];
         for (let j = 0; j < adjList[i].length; j++) {
-            const neighbor = adjList[i][j];
             neighbor = 0;
+        }
+    }
+
+    for(var i = 0; i < adjList.length; i++) {
+        for(var j = 0; j < adjList[i].length; j++) {
             adjMatrix[i][neighbor] = 1;
         }
     }
