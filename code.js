c82efe1757ff265637668f87513085cc2bceaa73
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-10 04:19:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function convertToAdjList(adjMatrix) {
     var adjList = [];
     for (var i = 0; i < adjMatrix.length; i++) {
         adjList[i] = [];
-        for (var j = 0; j < adjMatrix.length; i++) {
+        for (var j = 0; j < adjMatrix[i].length; i++) {
             if (adjMatrix[i][j] == 1) {
                 adjList[i].push(j);
             }
