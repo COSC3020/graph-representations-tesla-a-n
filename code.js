3fc51022780c19e85c0a287e0d991c1da7a9737b
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-10 03:05:18 -0600

    pseudocode

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    adjList of 0
+    outterloop for each vertex
+        innerloop for each vertex
+            push 1
+    return adjList;
 }
